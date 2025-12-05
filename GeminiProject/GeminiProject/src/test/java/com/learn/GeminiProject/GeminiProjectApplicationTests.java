e9128af547374b66ec4da011c371bbd1c2783bc2
Ss053 <158609185+Ss053@users.noreply.github.com>
2025-12-05 17:26:39 +0530

    initialized the project

---
+++ GeminiProject/GeminiProject/src/test/java/com/learn/GeminiProject/GeminiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.GeminiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeminiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
